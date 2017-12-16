80a33cd165bc18a04af2694348b7a9405eea61ec
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-12-16 19:58:12 +0100

    Complete day3 - part 2.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,9 @@
 use std::iter;
 use std::iter::Take;
 use std::iter::Repeat;
+use std::collections::HashMap;
+
+const INPUT: i32 = 361527;
 
 #[derive(Clone, Copy)]
 enum Direction {
@@ -65,10 +68,73 @@ fn distance(coords: (i32, i32)) -> i32 {
     x + y
 }
 
-fn main() {
-    let n = 361527;
-    let steps = n - 1; // We start with index 1.
+fn part1() {
+    let steps = INPUT - 1; // We start with index 1.
     let start = (0, 0);
     let coords = Spiral::new().flat_map(side_iter).take(steps as usize).fold(start, step);
     println!("Distance: {}", distance(coords));
+}
+
+struct Grid {
+    location: (i32, i32),
+    grid: HashMap<(i32, i32), i32>,
+}
+
+impl Grid {
+    fn new() -> Grid {
+        let mut grid = HashMap::new();
+        let start = (0, 0);
+        grid.insert(start, 1);
+        Grid {
+            location: start,
+            grid: grid,
+        }
+    }
+
+    fn step(&mut self, dir: Direction) -> i32 {
+        self.location = step(self.location, dir);
+        let (x, y) = self.location;
+        let surrounding = [
+            self.get((x-1, y-1)),
+            self.get((x-1, y)),
+            self.get((x-1, y+1)),
+            self.get((x, y-1)),
+            self.get((x, y+1)),
+            self.get((x+1,y-1)),
+            self.get((x+1, y)),
+            self.get((x+1, y+1)),
+        ].iter().sum();
+        self.set(surrounding);
+        return surrounding;
+    }
+
+    fn set(&mut self, value: i32) -> &mut Grid {
+        self.grid.insert(self.location, value);
+        self
+    }
+
+    fn get(&self, coords: (i32, i32)) -> i32 {
+        match self.grid.get(&coords) {
+            Some(x) => *x,
+            None => 0,
+        }
+    }
+}
+
+
+fn part2() {
+    let mut grid = Grid::new();
+    let result = Spiral::new()
+        .flat_map(side_iter)
+        .map(|dir| grid.step(dir))
+        .find(|v| v > &INPUT);
+    match result {
+        Some(output) => println!("Output: {}", output),
+        None => println!("Could not calculate output.!"),
+    }
+}
+
+fn main() {
+    part1();
+    part2()
 }
