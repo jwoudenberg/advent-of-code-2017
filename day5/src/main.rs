5b8b922b712be446174c9fde3c74a129f5465288
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-12-17 11:38:02 +0100

    Solve day5, part1

---
+++ day5/src/main.rs
@@ -0,0 +1,76 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+extern crate itertools;
+
+fn main() {
+    match part1() {
+        Ok(()) => (),
+        Err(err) => println!("Error: {}", err),
+    }
+}
+
+fn part1() -> Result<(), Box<Error>> {
+    let input = read_input()?;
+    let instructions: Vec<i32> = input.lines().map(parse_int).collect();
+    let initial = Zipper::from_vec(instructions).unwrap();
+    let iter = itertools::unfold(initial, step);
+    let jumps = iter.count() + 1;
+    println!("Jumps: {}", jumps);
+    Ok(())
+}
+
+struct Zipper<T> {
+    items: Vec<T>,
+    focus: usize
+}
+
+impl<T> Zipper<T> {
+    fn from_vec(vec: Vec<T>) -> Option<Zipper<T>> {
+        if vec.len() == 0 {
+            None
+        } else {
+            Some(
+                Zipper {
+                    items: vec,
+                    focus: 0,
+                }
+            )
+        }
+    }
+
+    fn get(&self) -> &T {
+        self.items.iter().nth(self.focus).unwrap()
+    }
+
+    fn set(&mut self, x: T) -> () {
+        self.items[self.focus] = x;
+    }
+
+    fn step(&mut self, n: i32) -> Option<usize> {
+        let new_focus = (self.focus as i32) + n;
+        if new_focus < 0 || (new_focus as usize) >= self.items.len() {
+            None
+        } else {
+            self.focus = new_focus as usize;
+            Some(new_focus as usize)
+        }
+    }
+}
+
+fn step(instructions: &mut Zipper<i32>) -> Option<usize> {
+    let instruction = instructions.get().clone();
+    instructions.set(instruction + 1);
+    instructions.step(instruction)
+}
+
+fn read_input() -> Result<String, Box<Error>> {
+    let mut f = File::open("input.txt")?;
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn parse_int(str: &str) -> i32 {
+    return str.parse().unwrap();
+}
