83850a9fa27f8caaf933a1e8a9b3b936e76f267c
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-12-17 18:20:03 +0100

    Solve day 7, part 2
    Not a nice solution though...

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -5,10 +5,12 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashSet;
+use std::collections::HashMap;
+use std::hash::Hash;
 use regex::Regex;
 
 fn main() {
-    match part1() {
+    match solution() {
         Ok(()) => (),
         Err(err) => println!("Error: {}", err),
     }
@@ -21,14 +23,77 @@ struct Program<'a> {
     supporting: Vec<&'a str>
 }
 
-fn part1() -> Result<(), Box<Error>> {
+fn solution() -> Result<(), Box<Error>> {
     let input = read_input()?;
-    let programs: Vec<Program> = input.lines().map(parse_line).collect();
-    let names: HashSet<&&str> = programs.iter().map(|p| &p.name).collect();
-    let supported: HashSet<&&str> = programs.iter().flat_map(|p| p.supporting.iter()).collect();
-    let root = names.difference(&supported).next().unwrap();
-    println!("Root: {}", root);
+    let programs: HashMap<&str, Program> = input.lines().map(parse_line).map(|p| (p.name, p)).collect();
+    let root = find_root(&programs);
+    println!("Root: {}", root.name);
+    let mut inbalanced: Vec<&Program> = programs.values()
+        .filter(|p| is_inbalanced(p, &programs))
+        .collect();
+    inbalanced.sort_by_key(|p| height(p, &programs));
+    let highest_inbalance = inbalanced.iter().next().unwrap();
+    let corrected_weight = balance(highest_inbalance, &programs);
+    println!("Corrected weight: {}", corrected_weight);
     Ok(())
+}
+
+
+fn balance(program: &Program, programs: &HashMap<&str, Program>) -> u32 {
+    let mut weights: HashMap<u32, Vec<&Program>> = HashMap::new();
+    for c in children(program, &programs).iter() {
+        add_to(&mut weights, weight(c, programs), c);
+    }
+    let (wrong_weight, wrong_child) = weights.iter().min_by_key(|&(_w, ps)| ps.len()).unwrap();
+    let (correct_weight, _) = weights.iter().max_by_key(|&(_w, ps)| ps.len()).unwrap();
+    wrong_child.iter().next().unwrap().weight + correct_weight - wrong_weight
+}
+
+fn height(program: &Program, programs: &HashMap<&str, Program>) -> u32 {
+    match parent(program, programs) {
+        None => 0,
+        Some(p) => height(p, programs),
+    }
+}
+
+fn parent<'a>(program: &Program<'a>, programs: &'a HashMap<&str, Program<'a>>) -> Option<&'a Program<'a>> {
+    programs.values().find(|p| p.supporting.contains(&program.name))
+}
+
+fn find_root<'a>(programs: &'a HashMap<&str, Program>) -> &'a Program<'a> {
+    let names: HashSet<&&str> = programs.keys().collect();
+    let supported: HashSet<&&str> = programs.values().flat_map(|p| p.supporting.iter()).collect();
+    let root = names.difference(&supported).next().unwrap();
+    programs.get(*root).unwrap()
+}
+
+fn is_inbalanced(program: &Program, programs: &HashMap<&str, Program>) -> bool {
+    let mut weights = HashMap::new();
+    children(program, programs).iter()
+        .map(|c| weight(c, programs))
+        .for_each(|w| increment_key(&mut weights, w));
+    weights.len() > 1
+}
+
+fn increment_key<T>(map: &mut HashMap<T, u32>, key: T) -> () where T: Eq + Hash {
+    let count = map.entry(key).or_insert(0);
+    *count += 1;
+}
+
+fn add_to<K, V>(map: &mut HashMap<K, Vec<V>>, key: K, value: V) -> () where K: Eq + Hash {
+    let vec: &mut Vec<V> = map.entry(key).or_insert(vec![]);
+    vec.push(value);
+}
+
+fn children<'a>(program: &Program<'a>, programs: &'a HashMap<&str, Program<'a>>) -> Vec<&'a Program<'a>> {
+    program.supporting.iter()
+        .map(|n| programs.get(n).unwrap())
+        .collect()
+}
+
+fn weight(program: &Program, programs: &HashMap<&str, Program>) -> u32 {
+    let child_weight: u32 = children(program, programs).iter().map(|p| weight(p, programs)).sum();
+    program.weight + child_weight
 }
 
 fn parse_line(line: &str) -> Program {
