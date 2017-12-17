05345b528d1901e61d1c1ef163fc1b704f5c25b9
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-12-17 09:29:13 +0100

    Complete day1, part 2

--- day1/main.rs
+++ day1/main.rs
@@ -13,16 +13,33 @@ fn get_input() -> Result<String, Box<Error>>{
     return Ok(input);
 }
 
-fn calculate_sum(input: String) -> u32 {
+fn calculate_sum(input: String, displace: usize) -> u32 {
     let numbers = input.trim().chars().map(to_digit);
-    let shifted = numbers.clone().cycle().skip(1);
+    let shifted = numbers.clone().cycle().skip(displace);
     let pairs = numbers.zip(shifted);
     return pairs.map(|(x, y)| if x == y { x } else { 0 }).sum();
 }
 
+fn part1() -> Result<(), Box<Error>> {
+    let input = get_input()?;
+    println!("Sum, part 1: {}", calculate_sum(input, 1));
+    Ok(())
+}
+
+fn part2() -> Result<(), Box<Error>> {
+    let input = get_input()?;
+    let half_size = input.len() / 2;
+    println!("Sum, part 2: {}", calculate_sum(input, half_size));
+    Ok(())
+}
+
 fn main() {
-    match get_input() {
-        Ok(input) => println!("{}", calculate_sum(input)),
-        Err(e) => println!("Error: {}", e.to_string()),
+    match part1() {
+        Ok(()) => (),
+        Err(err) => println!("Error: {}", err),
+    }
+    match part2() {
+        Ok(()) => (),
+        Err(err) => println!("Error: {}", err),
     }
 }
